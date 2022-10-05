b283b9a8e0f621272e70fa0544d60db3ff8923ae
jdgc14 <joseojedapro@gmail.com>
2022-10-05 17:20:24 -0500

    models and relations created

--- models/initModels.js
+++ models/initModels.js
@@ -1,6 +1,21 @@
 // Models
-const { User } = require('./user.model');
+const { User } = require('./user.model')
+const { Song } = require('./song.model')
+const { Album } = require('./album.model')
+const { Artist } = require('./artist.model')
 
-const initModels = () => {};
+const initModels = () => {
+    // 1 Artist <---> M Album
+    Artist.hasMany(Album, { foreignKey: 'artistId' })
+    Album.belongsTo(Artist)
 
-module.exports = { initModels };
+    // 1 Album <---> M Song
+    Album.hasMany(Song, { foreignKey: 'albumId' })
+    Song.belongsTo(Album)
+
+    // M User <---> M Song
+    User.belongsToMany(Song, { through: 'favoriteSong', foreignKey: 'userId' })
+    Song.belongsToMany(User, { through: 'favoriteSong', foreignKey: 'songId' })
+}
+
+module.exports = { initModels }
